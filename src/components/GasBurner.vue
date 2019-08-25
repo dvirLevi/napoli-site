4e5678616c80620877e536950d62717c31bef4c5
dvirLevi <dvirleviapp@gmail.com>
2019-08-25 15:12:14 +0300

    add

--- src/components/GasBurner.vue
+++ src/components/GasBurner.vue
@@ -1,46 +1,57 @@
 <template>
   <div class="row mt-3">
-     <div class="col-md-6 p-3 left-side center-all border-left">
-      
+    <div class="col-md-6 p-3 left-side center-all border-left">
+
       <div class="row center-all">
         <div class="col center-all">
-        <div class="block-text">
-          <h2>מבער פחמים</h2>
-          <p>ה-<span>Napoli Pizza Oven</span> מגיע עם תא נירוסטה מעוצב, גדול וייחודי להבערת פחמים, עצים או שבבים. התא
-            נפרד
-            מחלל האפייה
-            וניתן לשליפה בקלות. פתח הזנה בגג התנור מאפשר להוסיף בקלות עצים חדשים תוך כדי עבודה.</p>
-          <!-- <ButtonLink text="תוכלו לרכוש אותו כאן" link="/" /> -->
-        </div>
+          <div class="block-text">
+            <h2>מבער פחמים</h2>
+            <p>ה-<span>Napoli Pizza Oven</span> מגיע עם תא נירוסטה מעוצב, גדול וייחודי להבערת פחמים, עצים או שבבים. התא
+              נפרד
+              מחלל האפייה
+              וניתן לשליפה בקלות. פתח הזנה בגג התנור מאפשר להוסיף בקלות עצים חדשים תוך כדי עבודה.</p>
+            <!-- <ButtonLink text="תוכלו לרכוש אותו כאן" link="/" /> -->
+          </div>
         </div>
       </div>
       <div class="row">
         <div class="col center-all">
-        <img data-aos="fade-up" src="../assets/ass4.jpg" alt="">
+          <img data-aos="fade-up" src="../assets/ass4.jpg" alt="">
         </div>
       </div>
-      
+
     </div>
     <div class="col-md-6 p-3 left-side center-all ">
-       <div class="row">
-        <div class="col center-all">
-        <img data-aos="fade-up" src="../assets/ass8.png" alt="">
+      <div class="row row-gas">
+        <div class="col-md-12 center-all">
+          <img data-aos="fade-up" src="../assets/ass8.png" alt="">
+        </div>
+        <div class="col-md-12 center-all">
+          <div class="block-text">
+            <h2>מבער גז</h2>
+            <p>מבער הגז של <span>Napoli Pizza Oven</span> מספק אש חזקה, יציבה ונקייה, והוא חסכוני ביותר: כ-420 גרם בלבד
+              לשעת
+              בעירה. עוצמת הלהבות ניתנת לשליטה מדויקת. לחיצה אחת על מצת המבער, והתנור לוהט. מבער הגז ניתן לרכישה בנפרד.
+              <span>מומלץ!</span></p>
+            <ButtonLink text="לרכישת המבער" link="/" />
+          </div>
         </div>
       </div>
-      <div class="row center-all">
+      <!-- <div class="row center-all">
         <div class="col center-all">
           <div class="block-text">
-        <h2>מבער גז</h2>
-        <p>מבער הגז של <span>Napoli Pizza Oven</span> מספק אש חזקה, יציבה ונקייה, והוא חסכוני ביותר: כ-420 גרם בלבד לשעת
-          בעירה. עוצמת הלהבות ניתנת לשליטה מדויקת. לחיצה אחת על מצת המבער, והתנור לוהט. מבער הגז ניתן לרכישה בנפרד.
-          <span>מומלץ!</span></p>
-        <ButtonLink text="לרכישת המבער" link="/" />
-      </div>
+            <h2>מבער גז</h2>
+            <p>מבער הגז של <span>Napoli Pizza Oven</span> מספק אש חזקה, יציבה ונקייה, והוא חסכוני ביותר: כ-420 גרם בלבד
+              לשעת
+              בעירה. עוצמת הלהבות ניתנת לשליטה מדויקת. לחיצה אחת על מצת המבער, והתנור לוהט. מבער הגז ניתן לרכישה בנפרד.
+              <span>מומלץ!</span></p>
+            <ButtonLink text="לרכישת המבער" link="/" />
+          </div>
         </div>
-      </div>
-     
+      </div> -->
+
     </div>
-    
+
     <!-- <div class="col-md-6 center-all pb-3">
 
     </div>
@@ -100,6 +111,10 @@
     font-size: 24px;
   }
 
+  .row-gas {
+    flex-direction: column;
+  }
+
   @media (max-width: 767.98px) {
     .left-side img {
       width: 100%;
@@ -127,5 +142,8 @@
     p {
       font-size: 20px;
     }
+    .row-gas {
+    flex-direction: column-reverse;
+  }
   }
 </style>
