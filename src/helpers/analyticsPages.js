02f3a997b53a0619c5ec56ea7add57a68ae8c043
dvirLevi <dvirleviapp@gmail.com>
2019-10-30 21:58:39 +0200

    add remarketing google

--- src/helpers/analyticsPages.js
+++ src/helpers/analyticsPages.js
@@ -1,11 +1,30 @@
-const analyticsPages = (route)=> {
-    if(ifAlreadyAnalytics){
-        gtag('config', 'UA-150230848-1', {
-        'page_title': route,
-        'page_path': `/${route}`
-      });
-      }
-      ifAlreadyAnalytics = true;
+const analyticsPages = (route) => {
+  if (ifAlreadyAnalytics) {
+    gtag('config', 'UA-150230848-1', {
+      'page_title': route,
+      'page_path': `/${route}`
+    });
+    gtag('config', 'AW-698669401', {
+      'page_title': route,
+      'page_path': `/${route}`
+    });
+
+
+    // gtag('event', 'page_view', {
+    //   'send_to': 'AW-698669401',
+    //   'value': 'replace with value',
+    //   'items': [{
+    //     'id': 'replace with value',
+    //     'location_id': 'replace with value',
+    //     'google_business_vertical': 'local'
+    //   }, {
+    //     'id': 'replace with value',
+    //     'google_business_vertical': 'retail'
+    //   }]
+    // });
+
+  }
+  ifAlreadyAnalytics = true;
 }
 
 export default analyticsPages
