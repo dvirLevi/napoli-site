a973b75faf7bb354b42bce3f09e297cf42f80df8
dvirLevi <dvirleviapp@gmail.com>
2019-11-04 17:43:33 +0200

    add time

--- src/components/counterDays.vue
+++ src/components/counterDays.vue
@@ -48,7 +48,7 @@
     methods: {
       counter() {
         // Set the date we're counting down to
-        var countDownDate = new Date("nov 4, 2019 24:00:00").getTime();
+        var countDownDate = new Date("nov 5, 2019 10:00:00").getTime();
 
         // Update the count down every 1 second
         var x = setInterval(() => {
