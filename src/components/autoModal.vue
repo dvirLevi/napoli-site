acabe4e2c761c5f5af27dd71f3e5f72d22ae8e71
dvirLevi <dvirleviapp@gmail.com>
2020-10-15 20:22:48 +0300

    add

--- src/components/autoModal.vue
+++ src/components/autoModal.vue
@@ -6,31 +6,32 @@
     <div class="w-100 center-all">
       <img src="../assets/ass32.jpg" alt="">
     </div>
-    <h3 class="text-center mt-3">הוא הגיע!!!</h3>
+    <!-- <h3 class="text-center mt-3">הוא הגיע!!!</h3> -->
     <!-- <h3 class="text-center">מלאי חדש של תנורי נאפולי הגיע סוף-סוף לארץ!</h3> -->
     <!-- <h3 class="text-center font-weight-bold">עכשיו במבצע השקה לשבוע בלבד!</h3> -->
-    <h3 class="text-center font-weight-bold">הטאבון האהוב שלנו חזר למלאי! עכשיו במבצע מיוחד לשבוע בלבד!
-מהרו לרכוש לפני שהמלאי שוב יתחסל... 🙂</h3>
-    <!-- <div class="w-100 f-center"> -->
-    <!-- <ButtonLink class="h5" @customEvent="closeModal" text="לחנות" link="/store" /> -->
+    <h3 class="text-center mt-3">בצק משובח, רוטב עגבניות חריף, בזיליקום ריחני, מוצרלה טרייה, וקצת זיתי קלמטה למעלה. דקה וחצי בטאבון שלנו, ויש לכם פיצה איטלקית לוהטת ישר מגן עדן.</h3>
+    <h3 class="text-center font-weight-bold">אז אל תפספסו את המבצע שלנו, כי הוא תכף נגמר.</h3>
+    <router-link class="font-weight-bold c-p link-store" tag="h3" to="/store" @click.native="closeModal">רכשו עכשיו >></router-link>
+    <!-- <div class="w-100 f-center">
+    <ButtonLink class="h5" @customEvent="closeModal" text="לחנות" link="/store" />
 
-    <!-- </div> -->
-    <div class="w-100 center-all">
+    </div> -->
+    <!-- <div class="w-100 center-all">
       <h2 class="text-center">יש לכם שאלות? דברו איתנו</h2>
       <div class="wrap-w center-all w m-3">
         <a href="https://api.whatsapp.com/send?phone=972537234223" target="_blanc">
           <i class="fab fa-whatsapp"></i>
         </a>
       </div>
-      <!-- <div class="wrap-w center-all f m-3">
+      <div class="wrap-w center-all f m-3">
         <a href="https://m.me/napoli.oven.il">
           <i class="fab fa-facebook-messenger"></i>
         </a>
-      </div> -->
-      <!-- <div class="w-100 center-all">
+      </div>
+      <div class="w-100 center-all">
         <h2 class="text-center">וקבלו מיד קוד קופון מיוחד להנחה משתלמת!</h2>
-      </div> -->
-    </div>
+      </div>
+    </div> -->
   </Modal>
 </template>
 
@@ -77,6 +78,11 @@
 
   .w {
     background-color: #2cb742;
+  }
+
+  .link-store {
+    color: #0068f7;
+    text-decoration: underline;
   }
 
   @media (max-width: 767.98px) {
