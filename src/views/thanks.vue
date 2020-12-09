db8307c74e7a174792dc368b7e23878852219069
dvirLevi <dvirleviapp@gmail.com>
2020-12-09 11:26:19 +0200

    add

--- src/views/thanks.vue
+++ src/views/thanks.vue
@@ -2,7 +2,7 @@
   <div class="container min-height-screen f-center">
     <div class="row">
       <div class="col f-center">
-        <h2 class="text-center">תודה שרכשתם תנור ברטלו!</h2>
+        <h2 class="text-center">תודה שרכשת את מוצרי ברטלו</h2>
         <!-- <div class="w-100 f-center">
           <h4>בעוד מס' שניות תועבר לאזור האישי שלך...</h4>
         </div> -->
