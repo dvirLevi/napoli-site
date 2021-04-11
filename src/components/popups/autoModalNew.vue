ef447be39bd7e61652c755f4418dd84b05a175ff
dvirLevi <dvirleviapp@gmail.com>
2021-04-11 15:14:47 +0300

    add

--- src/components/popups/autoModalNew.vue
+++ src/components/popups/autoModalNew.vue
@@ -1,26 +1,29 @@
 <template>
-  <Modal :myStyle="{backgroundImage: `url(${require('@/assets/ass28.jpg')})`}">
-    <div class="w-100 p-4">
-      <div class="w-100 right-all">
-        <i class="las la-times h3 c-p text-white" @click="closeModal"></i>
+  <Modal>
+    <div class="w-100 p-4 bg-white position-relative">
+      <div class="bg flag img-cover"></div>
+
+      <div class="w-100 content">
+        <div class="w-100 right-all">
+          <i class="las la-times h3 c-p" @click="closeModal"></i>
+        </div>
+        <a class="w-100 c-p" href="https://api.whatsapp.com/send?phone=972537234223" target="_blanc">
+          <div class="w-100 f-center-top">
+            <div class="text mt-md-0 mt-3">
+              <h1 class="color">חוגגים עצמאות בברטלו ישראל!</h1>
+              <h2>מבצע כביר בחנות האתר עד מוצאי החג!</h2>
+            </div>
+            <div class="p-img">
+              <img class="w-100" src="@/assets/ass29.jpg" alt="">
+            </div>
+          </div>
+          <div class="w-100 f-center mt-4">
+            <h1 class="text-center">לרכישה >></h1>
+          </div>
+        </a>
+    <firework />
+
       </div>
-      <a class="w-100 c-p" href="https://api.whatsapp.com/send?phone=972537234223" target="_blanc">
-        <div class="w-100 f-center-top">
-          <div class="text mt-md-0 mt-3">
-            <h1 class="color">אחחח איזה ריח!</h1>
-            <h2>גם אתם רוצים לאפות פיצות איטלקיות לוהטות בתנור המהמם שלנו?</h2>
-          </div>
-          <div class="p-img">
-            <img class="w-100" src="@/assets/ass29.jpg" alt="">
-          </div>
-        </div>
-        <div class="w-100 f-center mt-4">
-          <h2 class="text-center">שלחו לנו וואטספ עם המילים <span class="color">קופון הנחה</span> ונפנק אתכם</h2>
-          <span class="wrap-w f-center">
-          <i class="fab fa-whatsapp"></i>
-          </span>
-        </div>
-      </a>
     </div>
   </Modal>
 </template>
@@ -28,13 +31,13 @@
 <script>
   // @ is an alias to /src
   import Modal from '@/components/Modal.vue'
-  // import firework from '@/components/firework.vue'
+  import firework from '@/components/firework.vue'
 
   export default {
     name: 'autoModalNew',
     components: {
-      Modal
-      // firework
+      Modal,
+      firework
     },
     props: {
 
@@ -48,41 +51,51 @@
 </script>
 
 <style scoped>
+  .content {
+    position: relative;
+    z-index: 2;
+  }
+
   .text {
     width: 66%;
   }
 
   .color {
-    color: #60d66a;
+    color: #2562BD;
   }
 
   .p-img {
     width: 33%;
-    border: solid 5px #60d66a;
+    border: solid 5px #2562BD;
     padding: 5px
   }
 
   h1 {
     font-size: 3.2vw;
-    font-weight: bold;
-    color: #fff;
+    font-weight: bolder;
   }
 
   h2 {
     font-weight: bold;
-    color: #fff;
+    /* color: #444; */
   }
 
-    .wrap-w {
+  .wrap-w {
     width: 70px;
     height: 70px;
     border-radius: 100px;
-    box-shadow: 0 0 14px var(--main-color)bd;
+    box-shadow: 0 0 14px var(--main-color);
     background-color: #2cb742;
   }
-   .wrap-w i {
+
+  .wrap-w i {
     font-size: 50px;
     color: #ffffff;
+  }
+
+  .flag {
+    background-image: url('../../assets/ass35.jpg');
+    opacity: 0.3;
   }
 
   @media (max-width: 767.98px) {
