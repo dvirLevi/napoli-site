7cf036e5bc9a18cd9b0dc72999a0a2cbdb4f1ff0
dvirLevi <dvirleviapp@gmail.com>
2021-07-07 11:24:10 +0300

    add

--- src/components/popups/autoModal.vue
+++ src/components/popups/autoModal.vue
@@ -2,37 +2,37 @@
   <Modal>
     <div class="row">
       <div class="col">
-        dfgdfg
-      </div>
-      <!-- <div class="w-100 right-all">
-        <span class="h3 c-p" @click="closeModal">x</span>
-      </div>
-      <div class="w-100 center-all">
-        <img src="../assets/ass32.jpg" alt="">
-      </div>
-      <div>
-        <h3 class="text-center mt-3 font-weight-bolder">חיכינו וחיכינו, והנה זה הגיע!!!</h3>
-        <h3 class="text-center mt-3 ">מלאי חדש של תנורי ברטלו הגיע סוף-סוף לארץ!</h3>
-        <h3 class="text-center mt-3">עכשיו במבצע השקה לשבוע בלבד!</h3>
-        <h3 class="text-center mt-3">מהרו לרכוש לפני שהמלאי שוב יתחסל... 🙂</h3>
-      </div>
+        <div class="w-100 right-all p-3">
+          <span class="h3 c-p" @click="closeModal">x</span>
+        </div>
+        <div class="w-100 center-all">
+          <img src="@/assets/ass32.jpg" alt="">
+        </div>
+        <div>
+          <h3 class="text-center mt-3 font-weight-bolder">נגמר לנו המלאי 😞</h3>
+          <!-- <h3 class="text-center mt-3 ">מלאי חדש של תנורי ברטלו הגיע סוף-סוף לארץ!</h3>
+          <h3 class="text-center mt-3">עכשיו במבצע השקה לשבוע בלבד!</h3>
+          <h3 class="text-center mt-3">מהרו לרכוש לפני שהמלאי שוב יתחסל... 🙂</h3> -->
+        </div>
 
-      <h3 class="text-center font-weight-bold">אז אל תפספסו את המבצע שלנו, כי הוא תכף נגמר.</h3>
-      <router-link class="font-weight-bold c-p link-store text-center" tag="h2" to="/store" @click.native="closeModal">
-        רכשו עכשיו >></router-link>
-      <div class="w-100 center-all">
-        <h3 class="text-center">דברו איתנו - נשמח לענות על כל שאלה</h3>
-        <div class="wrap-w center-all w m-3">
-        <a href="https://api.whatsapp.com/send?phone=972537234223" target="_blanc">
-          <i class="fab fa-whatsapp"></i>
-        </a>
+        <!-- <h3 class="text-center font-weight-bold">אז אל תפספסו את המבצע שלנו, כי הוא תכף נגמר.</h3>
+        <router-link class="font-weight-bold c-p link-store text-center" tag="h2" to="/store"
+          @click.native="closeModal">
+          רכשו עכשיו >></router-link> -->
+        <div class="w-100 center-all">
+          <h3 class="text-center">שלחו לנו וואטספ ונעדכן אתכם ראשונים מיד כשהמלאי יתחדש! </h3>
+          <div class="wrap-w center-all w m-3">
+            <a href="https://api.whatsapp.com/send?phone=972537234223" target="_blanc">
+              <i class="fab fa-whatsapp"></i>
+            </a>
+          </div>
+          <!-- <div class="wrap-w center-all f m-3">
+            <a href="https://m.me/napoli.oven.il">
+              <i class="fab fa-facebook-messenger"></i>
+            </a>
+          </div> -->
+        </div>
       </div>
-        <div class="wrap-w center-all f m-3">
-        <a href="https://m.me/napoli.oven.il">
-          <i class="fab fa-facebook-messenger"></i>
-        </a>
-      </div>
-      </div> -->
     </div>
   </Modal>
 </template>
